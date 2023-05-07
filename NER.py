26f855d1c628a10287293b1ec6aa9b1ad3b341c5
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 02:46:45 -0700

    Update NER.py

--- NER.py
+++ NER.py
@@ -1,5 +1,6 @@
 # Inspiration: https://blog.jcharistech.com/2020/07/09/simple-nlp-app-with-spacy-streamlit/
 
+import spacy
 import spacy_streamlit
 spacy.download('en_core_web_sm')
 spacy.download('en_core_web_md')
