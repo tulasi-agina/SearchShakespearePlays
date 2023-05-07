b717105ceb5fea3259689952afa147eb5872bdc5
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-06 23:11:29 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,3 +7,20 @@ def load_data():
     df = pd.read_csv(file)
     return df
     
+file = "../shakespeare_data.csv"
+df = load_data()
+create_data = {"Dataline": "number",
+                "Play": "multiselect",
+                "PlayerLinenumber": "number",
+                "ActSceneLine": "text",
+                "Player": "text",
+                "PlayerLine": "text"}
+
+all_widgets = sp.create_widgets(df, create_data, ignore_columns=["Dataline"])
+res = sp.filter_df(df, all_widgets)
+st.title("Streamlit AutoPandas")
+st.header("Original DataFrame")
+st.write(df)
+
+st.header("Result DataFrame")
+st.write(res)
