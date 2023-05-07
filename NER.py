801ffe0e99bafabd3260964d240c7a568b61b385
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 01:46:57 -0700

    Update NER.py

--- NER.py
+++ NER.py
@@ -1,2 +1,10 @@
 # Inspiration: https://blog.jcharistech.com/2020/07/09/simple-nlp-app-with-spacy-streamlit/
 
+import streamlit as st
+import spacy_streamlit
+import spacy
+
+import os
+from PIL import Image
+
+nlp = spacy.lead('en')
