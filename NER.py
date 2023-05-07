f0b46b7fa0f30e9f386e2967369dda98b5e7b72f
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 02:46:15 -0700

    Update NER.py

--- NER.py
+++ NER.py
@@ -1,6 +1,8 @@
 # Inspiration: https://blog.jcharistech.com/2020/07/09/simple-nlp-app-with-spacy-streamlit/
 
 import spacy_streamlit
+spacy.download('en_core_web_sm')
+spacy.download('en_core_web_md')
 
 models = ["en_core_web_sm", "en_core_web_md"]
 default_text = "Sundar Pichai is the CEO of Google."
