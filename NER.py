2d8af7e71c4e6fa5c2cbc7e26cc85668f371287c
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 02:42:15 -0700

    Update NER.py

--- NER.py
+++ NER.py
@@ -1,18 +1,7 @@
 # Inspiration: https://blog.jcharistech.com/2020/07/09/simple-nlp-app-with-spacy-streamlit/
 
-import streamlit as st
 import spacy_streamlit
-import spacy
 
 models = ["en_core_web_sm", "en_core_web_md"]
-
-"""A Simple NLP app with Spacy-Streamlit"""
-
-st.title("Named Entity Recognition")
-
-import spacy
-from spacy_streamlit import visualize_parser
-
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("This is a text")
-visualize_parser(doc)
+default_text = "Sundar Pichai is the CEO of Google."
+spacy_streamlit.visualize(models, default_text))
