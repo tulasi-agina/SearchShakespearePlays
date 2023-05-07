b03cb6c80a856fb879dc34daba90ae51a0c1a3d6
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2023-05-07 10:26:43 -0700

    Update RandomLine.py

--- RandomLine.py
+++ RandomLine.py
@@ -1,9 +1,10 @@
 import random
 import streamlit as st
+import pandas as pd
 
 st.write("Hello")
 
 file = "Shakespeare_data.csv"
-df = load_data()
+df = pd.load_data(file)
 
 st.write(df.iat[4, 2])
